28470dc6339f6df7c995d6e31f9beee2b6f4a62b
Ilya QA-58 <ilyamaidanov58@yandex.ru>
2023-06-29 14:59:55 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/page/TopupPage.java
@@ -0,0 +1,21 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class TopupPage {
+    private SelenideElement heading= $("[data-test-id='dashboard']");
+
+    public TopupPage() {
+        heading.shouldBe(Condition.visible);
+    }
+    public DashboardPage transfer(String cardNumber, String amount) {
+        $("[data-test-id='amount'] input").setValue(amount);
+        $("[data-test-id='from'] input").setValue(cardNumber);
+        $("[data-test-id='action-transfer']").click();
+        return new DashboardPage();
+    }
+}
---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,61 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
+
+
+public class MoneyTransferTest {
+  int initBalCardOne;
+  int initBalCardTwo;
+  int finBalCardOne;
+  int finBalCardTwo;
+  DashboardPage dashboardPage;
+
+
+  @BeforeEach
+  void SetUp() {
+    var loginPage = open("http://localhost:9999", LoginPage.class);
+    var loginData = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(loginData);
+    var verificationCode = getVerificationCodeFor(loginData);
+    dashboardPage = verificationPage.validVerify(verificationCode);
+    initBalCardOne = dashboardPage.getFirstCardBalance();
+    initBalCardTwo = dashboardPage.getSecondCardBalance();
+  }
+
+
+  @Test
+  void shouldTransferFromSecondToFirstCard() {
+    int amount = 2500;
+    var secondCardInfo = DataHelper.getSecondCardInfo();
+    var topupPage = dashboardPage.transferPage2();
+    dashboardPage = topupPage.transfer(secondCardInfo.getCardNumber(), Integer.toString(amount));
+    finBalCardOne = dashboardPage.getFirstCardBalance();
+    finBalCardTwo = dashboardPage.getSecondCardBalance();
+    assertEquals(initBalCardOne + amount, finBalCardOne);
+    assertEquals(initBalCardTwo - amount, finBalCardTwo);
+  }
+
+  @Test
+  void shouldTransferFromFirstToSecondCard() {
+    int amount = 2500;
+    var firstCardInfo = DataHelper.getFirstCardInfo();
+    var topupPage = dashboardPage.transferPage1();
+    dashboardPage = topupPage.transfer(firstCardInfo.getCardNumber(), Integer.toString(amount));
+    finBalCardOne = dashboardPage.getFirstCardBalance();
+    finBalCardTwo = dashboardPage.getSecondCardBalance();
+    assertEquals(initBalCardOne - amount, finBalCardOne);
+    assertEquals(initBalCardTwo + amount, finBalCardTwo);
+  }
+
+}
+
+
